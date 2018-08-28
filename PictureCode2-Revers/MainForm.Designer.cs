94eb69a41a49e4dcc961253b35693838c7f16bfa
AndreiHomiak <andrei-homiak@mail.ru>
2018-08-28 10:20:17 +0300

    First commit

---
+++ PictureCode2-Revers/MainForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: KAMIAK
+ * Дата: 06.08.2018
+ * Время: 14:39
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace PictureCode2_Revers
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox txtCode;
+		private System.Windows.Forms.Button btnConvert;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.TextBox txtWidth;
+		private System.Windows.Forms.TextBox txtHeight;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.txtCode = new System.Windows.Forms.TextBox();
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.txtWidth = new System.Windows.Forms.TextBox();
+			this.txtHeight = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtCode
+			// 
+			this.txtCode.Location = new System.Drawing.Point(13, 13);
+			this.txtCode.Multiline = true;
+			this.txtCode.Name = "txtCode";
+			this.txtCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtCode.Size = new System.Drawing.Size(396, 273);
+			this.txtCode.TabIndex = 0;
+			// 
+			// btnConvert
+			// 
+			this.btnConvert.Location = new System.Drawing.Point(415, 12);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(75, 23);
+			this.btnConvert.TabIndex = 1;
+			this.btnConvert.Text = "Convert";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.BtnConvertClick);
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(415, 41);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 2;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.BtnSaveClick);
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.Location = new System.Drawing.Point(496, 12);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(308, 274);
+			this.pictureBox.TabIndex = 3;
+			this.pictureBox.TabStop = false;
+			// 
+			// txtWidth
+			// 
+			this.txtWidth.Location = new System.Drawing.Point(415, 120);
+			this.txtWidth.Name = "txtWidth";
+			this.txtWidth.Size = new System.Drawing.Size(75, 20);
+			this.txtWidth.TabIndex = 4;
+			// 
+			// txtHeight
+			// 
+			this.txtHeight.Location = new System.Drawing.Point(415, 165);
+			this.txtHeight.Name = "txtHeight";
+			this.txtHeight.Size = new System.Drawing.Size(75, 20);
+			this.txtHeight.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(416, 98);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 19);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Width";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(415, 143);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 19);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Height";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(415, 71);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(75, 24);
+			this.checkBox1.TabIndex = 8;
+			this.checkBox1.Text = "Size";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(816, 298);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtHeight);
+			this.Controls.Add(this.txtWidth);
+			this.Controls.Add(this.pictureBox);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.btnConvert);
+			this.Controls.Add(this.txtCode);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "PictureCode2-Revers";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ PictureCode2-Revers/MainForm.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: KAMIAK
+ * Дата: 06.08.2018
+ * Время: 14:39
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PictureCode2_Revers
+{
+	public partial class MainForm : Form
+	{
+		Bitmap pict;
+		List<byte> values_list;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			txtHeight.Enabled = false;
+			txtWidth.Enabled = false;
+			values_list = new List<byte>();
+		}
+		void BtnConvertClick(object sender, EventArgs e)
+		{
+			if(string.IsNullOrEmpty(txtCode.Text)==true)
+			{
+				MessageBox.Show("Пустое поле ввода", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			string ful_str = txtCode.Text;
+			ful_str = ful_str.Trim(',');
+			ful_str = ful_str.Trim();
+			string[] numbers_str = ful_str.Split(',');
+			for(int i = 0; i < numbers_str.Length; i++)
+			{
+				numbers_str[i] = numbers_str[i].Trim(Environment.NewLine.ToCharArray());
+				numbers_str[i] = numbers_str[i].Trim();
+			}
+			try 
+			{
+				if(checkBox1.Checked == false)
+				{
+					int height = Convert.ToInt32(numbers_str[0], 16);
+					int buf = Convert.ToInt32(numbers_str[1], 16);
+					height = height | (buf<<8);
+					int width = Convert.ToInt32(numbers_str[2], 16)*8;
+					width = width | ((Convert.ToInt32(numbers_str[3], 16)*8)<<8);
+					pict = new Bitmap(width, height);
+					
+					for(int i = 4; i < numbers_str.Length; i++)
+						values_list.Add(Convert.ToByte(numbers_str[i], 16));
+				}
+				else
+				{
+					pict = new Bitmap(Convert.ToInt32(txtWidth.Text), Convert.ToInt32(txtHeight.Text));
+					for(int i = 0; i < numbers_str.Length; i++)
+						values_list.Add(Convert.ToByte(numbers_str[i], 16));
+				}
+				
+				int cur_x=0, cur_y=0;
+				for(int i = 0; i < values_list.Count; i++)
+				{
+					byte bit8 = values_list[i];
+					UInt16 m = 128;
+					for(int k = 0; k < 8; k++)
+					{
+						if((bit8&m)==0)
+							pict.SetPixel(cur_x+k, cur_y, Color.White);
+						else
+							pict.SetPixel(cur_x+k, cur_y, Color.Black);
+						m/=2;
+					}
+					cur_x+=8;
+					if(cur_x>=pict.Width)
+					{
+						cur_x=0;
+						cur_y++;
+					}
+				}
+			}
+			catch(Exception)
+			{	
+				MessageBox.Show("Неверная строка ввода/не введены размеры изображения", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			pictureBox.Image = pict;
+			values_list.Clear();
+		}
+		void BtnSaveClick(object sender, EventArgs e)
+		{
+			saveFileDialog1.Filter = "Bitmap|*.bmp";
+			if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				pict.Save(saveFileDialog1.FileName);
+			}
+		}
+		void CheckBox1CheckedChanged(object sender, EventArgs e)
+		{
+			if(checkBox1.Checked == true)
+			{
+				txtWidth.Enabled = true;
+				txtHeight.Enabled = true;
+			}
+			else
+			{
+				txtWidth.Text = "";
+				txtHeight.Text = "";
+				txtWidth.Enabled = false;
+				txtHeight.Enabled = false;
+			}
+		}
+	}
+}
